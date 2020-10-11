0827966de42f75046aa942946ea22e7cfb9bdb68
wTonyChen <wTonyChen@users.noreply.github.com>
2020-10-11 20:05:00 +0800

    0.7.13/0.1.6-0.7.13

--- flatnmusic.gf.js
+++ flatnmusic.gf.js
@@ -6,7 +6,7 @@
 // @name 适用于网易云音乐扁平风格样式表的辅助用户脚本
 // @description 为网易云音乐扁平风格样式表提供更佳的用户体验。
 // @namespace wTonyChen.flatnmusich
-// @version 0.1.5-0.7.12
+// @version 0.1.6-0.7.13
 // @author wTonyChen (https://wtonychen.github.io)
 // @compatible chrome 83+
 // @compatible edge 83+
@@ -23,42 +23,42 @@
 
 !(function (e) {
   var t = {};
-  function n(l) {
-    if (t[l]) return t[l].exports;
-    var a = (t[l] = { i: l, l: !1, exports: {} });
-    return e[l].call(a.exports, a, a.exports, n), (a.l = !0), a.exports;
+  function a(n) {
+    if (t[n]) return t[n].exports;
+    var l = (t[n] = { i: n, l: !1, exports: {} });
+    return e[n].call(l.exports, l, l.exports, a), (l.l = !0), l.exports;
   }
-  (n.m = e),
-    (n.c = t),
-    (n.d = function (e, t, l) {
-      n.o(e, t) || Object.defineProperty(e, t, { enumerable: !0, get: l });
+  (a.m = e),
+    (a.c = t),
+    (a.d = function (e, t, n) {
+      a.o(e, t) || Object.defineProperty(e, t, { enumerable: !0, get: n });
     }),
-    (n.r = function (e) {
+    (a.r = function (e) {
       "undefined" != typeof Symbol &&
         Symbol.toStringTag &&
         Object.defineProperty(e, Symbol.toStringTag, { value: "Module" }),
         Object.defineProperty(e, "__esModule", { value: !0 });
     }),
-    (n.t = function (e, t) {
-      if ((1 & t && (e = n(e)), 8 & t)) return e;
+    (a.t = function (e, t) {
+      if ((1 & t && (e = a(e)), 8 & t)) return e;
       if (4 & t && "object" == typeof e && e && e.__esModule) return e;
-      var l = Object.create(null);
+      var n = Object.create(null);
       if (
-        (n.r(l),
-        Object.defineProperty(l, "default", { enumerable: !0, value: e }),
+        (a.r(n),
+        Object.defineProperty(n, "default", { enumerable: !0, value: e }),
         2 & t && "string" != typeof e)
       )
-        for (var a in e)
-          n.d(
+        for (var l in e)
+          a.d(
+            n,
             l,
-            a,
             function (t) {
               return e[t];
-            }.bind(null, a)
+            }.bind(null, l)
           );
-      return l;
+      return n;
     }),
-    (n.n = function (e) {
+    (a.n = function (e) {
       var t =
         e && e.__esModule
           ? function () {
@@ -67,13 +67,13 @@
           : function () {
               return e;
             };
-      return n.d(t, "a", t), t;
+      return a.d(t, "a", t), t;
     }),
-    (n.o = function (e, t) {
+    (a.o = function (e, t) {
       return Object.prototype.hasOwnProperty.call(e, t);
     }),
-    (n.p = ""),
-    n((n.s = 0));
+    (a.p = ""),
+    a((a.s = 0));
 })([
   function (e, t) {
     !(function () {
@@ -83,40 +83,41 @@
         ((e = document.createElement("div")),
         (e.className = "wk-fnm-hi-cfg"),
         document.body.appendChild(e));
-      let t = getComputedStyle(e),
+      let t,
+        a = getComputedStyle(e),
         n = (e) => {
-          let n = t.getPropertyValue("--" + e);
-          if (n)
+          let t = a.getPropertyValue("--" + e);
+          if (t)
             try {
-              n &&
-                n.indexOf("/") > -1 &&
-                (n = decodeURIComponent(n.replace(/\//g, "%")));
+              t &&
+                t.indexOf("/") > -1 &&
+                (t = decodeURIComponent(t.replace(/\//g, "%")));
             } catch (e) {}
           return (
-            ((0 === n.indexOf('"') && n.lastIndexOf('"') === n.length - 1) ||
-              (0 === n.indexOf("'") && n.lastIndexOf("'") === n.length - 1)) &&
-              (n = n.substr(1, n.length - 2)),
-            n
+            ((0 === t.indexOf('"') && t.lastIndexOf('"') === t.length - 1) ||
+              (0 === t.indexOf("'") && t.lastIndexOf("'") === t.length - 1)) &&
+              (t = t.substr(1, t.length - 2)),
+            t
           );
         },
         l = (e) => {
           let t = void 0;
           return window.localStorage && (t = localStorage.getItem(e)), t;
         },
-        a = n("wkhi-a"),
-        r = n("wkhi-b");
-      if (!r) return void e.parentElement.removeChild(e);
+        s = n("wkhi-a"),
+        i = n("wkhi-b");
+      if (!i) return void e.parentElement.removeChild(e);
       document.documentElement.setAttribute("wk-style-assist", "true");
-      let o = l("wkoptin") || !1,
-        s = function () {
+      let r = l("wkoptin") || !1,
+        o = function () {
           let e = arguments[0];
           for (let t = 1; t < arguments.length; t++) {
-            let n = RegExp("\\{" + t + "\\}", "g");
-            e = e.replace(n, arguments[t]);
+            let a = RegExp("\\{" + t + "\\}", "g");
+            e = e.replace(a, arguments[t]);
           }
           return e;
         };
-      o &&
+      r &&
         ((DocumentFragment.prototype.appendChildHost =
           DocumentFragment.prototype.appendChild),
         (DocumentFragment.prototype.appendChild = function () {
@@ -133,17 +134,17 @@
         l("wksfullpl") &&
           ((e) => {
             let t = n("wkhi-c"),
-              l = n("wkhi-d");
-            if (!t || !l) return;
-            (t = t.split("|")), (l = l.split("|"));
+              a = n("wkhi-d");
+            if (!t || !a) return;
+            (t = t.split("|")), (a = a.split("|"));
             for (let e = 0; e < t.length; e++)
-              for (let n = 0; n < l.length; n++)
-                (a = t[e]),
-                  (r = l[n]),
-                  (document.cookie = `${a};path=${r};max-age=3153600000`);
-            var a, r;
+              for (let n = 0; n < a.length; n++)
+                (l = t[e]),
+                  (s = a[n]),
+                  (document.cookie = `${l};path=${s};max-age=3153600000`);
+            var l, s;
           })();
-      let i = (e) => {
+      let c = (e) => {
         if (
           (l("wkshiresimages") &&
             ((e) => {
@@ -152,21 +153,21 @@
                 t[e].dataset.src &&
                   "" == t[e].src &&
                   (t[e].src = t[e].dataset.src);
-              let n = window.document.images;
-              for (let e = 0; e < n.length; e++) {
-                let t = n[e].src.split("?")[0];
-                if (n[e].src != t) {
-                  let l = new Image(),
-                    a = (r) => {
-                      n[e] &&
-                        (n[e].src.split("?")[0] == t &&
-                          ((n[e].src = t),
-                          n[e].classList.add("wk-hires-loaded")),
-                        l.removeEventListener("load", a, !1));
+              let a = window.document.images;
+              for (let e = 0; e < a.length; e++) {
+                let t = a[e].src.split("?")[0];
+                if (a[e].src != t) {
+                  let n = new Image(),
+                    l = (s) => {
+                      a[e] &&
+                        (a[e].src.split("?")[0] == t &&
+                          ((a[e].src = t),
+                          a[e].classList.add("wk-hires-loaded")),
+                        n.removeEventListener("load", l, !1));
                     };
-                  l.addEventListener("load", a, !1),
-                    n[e].classList.remove("wk-hires-loaded"),
-                    (l.src = t);
+                  n.addEventListener("load", l, !1),
+                    a[e].classList.remove("wk-hires-loaded"),
+                    (n.src = t);
                 }
               }
             })(),
@@ -180,12 +181,12 @@
               t &&
               (document.documentElement.classList.add("has-upb"),
               (t.style.cssText = `--upb:url("${e.src}")`));
-            let n = document.querySelector(".m-proifo .name .tit"),
-              l = document.querySelector(".m-proifo .name #j-name-wrap");
-            n &&
-              l &&
-              ((l.dataset.wkUsernameFull = l.title = n.innerText),
-              l.classList.add("wk-full-username"));
+            let a = document.querySelector(".m-proifo .name .tit"),
+              n = document.querySelector(".m-proifo .name #j-name-wrap");
+            a &&
+              n &&
+              ((n.dataset.wkUsernameFull = n.title = a.innerText),
+              n.classList.add("wk-full-username"));
           }
           let e = document.querySelector(".g-wrap>.m-info .cover img"),
             t = document.querySelector(".g-wrap>.m-info");
@@ -194,49 +195,148 @@
             (document.documentElement.classList.add("has-mib"),
             (t.style.cssText = `--mib:url("${e.src}")`));
         }
-        if (l("wksmusicsessionmeta") && "mediaSession" in navigator) {
-          let e = document.querySelector(".m-playbar .words .name"),
-            t = document.querySelector(".m-playbar .words .by"),
-            n = document.querySelector(".m-playbar .head img"),
-            l = [],
-            a = document.querySelector(".m-playbar .btns .prv"),
-            r = document.querySelector(".m-playbar .btns .ply"),
-            o = document.querySelector(".m-playbar .btns .nxt");
-          n && n.src && l.push([{ src: n.src.split("?")[0] }]),
-            (navigator.mediaSession.metadata = new MediaMetadata({
-              title: e ? e.innerText : document.title,
-              artist: e ? t.innerText : "",
-              artwork: l,
-            })),
-            navigator.mediaSession.setActionHandler(
-              "play",
-              (e) => !!r && r.click()
+        let a =
+            location.hash &&
+            (0 == location.hash.indexOf("#/video") ||
+              0 == location.hash.indexOf("#/mv")),
+          n = "/" == location.pathname,
+          s =
+            0 == location.pathname.indexOf("/video") ||
+            0 == location.pathname.indexOf("/mv");
+        if (
+          l("wksmusicsessionmeta") &&
+          "mediaSession" in navigator &&
+          (s || (n && !a))
+        ) {
+          let a = document.querySelector(
+              s ? ".n-mv .title h2" : ".m-playbar .words .name"
             ),
+            n = document.querySelector(
+              s ? ".n-mv .title .name" : ".m-playbar .words .by"
+            ),
+            l = document.querySelector(
+              s ? ".m-ctvideo .poster .pic" : ".m-playbar .head img"
+            ),
+            i = [],
+            r = document.querySelector(s ? null : ".m-playbar .btns .prv"),
+            o = document.querySelector(
+              s
+                ? ".m-ctvideo.z-play .controls .wrap .play"
+                : ".m-playbar .btns .ply"
+            ),
+            c = document.querySelector(s ? null : ".m-playbar .btns .nxt"),
+            d = document.querySelector(
+              s ? ".m-ctvideo" : ".m-playbar .btns .ply"
+            );
+          (d = d
+            ? d.classList.contains(s ? "z-play" : "pas")
+              ? "playing"
+              : d.classList.contains(s ? "z-pause" : "ply")
+              ? "paused"
+              : "none"
+            : "none"),
+            l &&
+              l.src &&
+              (i = [
+                {
+                  src: l.src.split("?")[0] + "?param=96y96",
+                  sizes: "96x96",
+                  type: "image/jpeg",
+                },
+                {
+                  src: l.src.split("?")[0] + "?param=128y128",
+                  sizes: "128x128",
+                  type: "image/jpeg",
+                },
+                {
+                  src: l.src.split("?")[0] + "?param=192y192",
+                  sizes: "192x192",
+                  type: "image/jpeg",
+                },
+                {
+                  src: l.src.split("?")[0] + "?param=256y256",
+                  sizes: "256x256",
+                  type: "image/jpeg",
+                },
+                {
+                  src: l.src.split("?")[0] + "?param=384y384",
+                  sizes: "384x384",
+                  type: "image/jpeg",
+                },
+                {
+                  src: l.src.split("?")[0] + "?param=512y512",
+                  sizes: "512x512",
+                  type: "image/jpeg",
+                },
+                { src: l.src.split("?")[0], sizes: "any", type: "image/jpeg" },
+              ]);
+          try {
+            let e = {
+              title: a ? a.innerText : document.title,
+              artist: n ? n.innerText : "",
+              album: n ? n.innerText : "",
+              artwork: i,
+            };
+            !((e, t) => {
+              if ("object" == typeof e)
+                try {
+                  e = JSON.stringify(e);
+                } catch (e) {}
+              if ("object" == typeof t)
+                try {
+                  t = JSON.stringify(t);
+                } catch (e) {}
+              return e == t;
+            })(e, t) &&
+              s &&
+              ((t = e),
+              (navigator.mediaSession.metadata = new MediaMetadata(t)));
+          } catch (e) {}
+          navigator.mediaSession.setActionHandler(
+            "play",
+            o
+              ? (e) => {
+                  o && o.click();
+                }
+              : null
+          ),
             navigator.mediaSession.setActionHandler(
               "pause",
-              (e) => !!r && r.click()
+              o
+                ? (e) => {
+                    o && o.click();
+                  }
+                : null
             ),
             navigator.mediaSession.setActionHandler(
               "previoustrack",
-              (e) => !!a && a.click()
+              r
+                ? (e) => {
+                    r && r.click();
+                  }
+                : null
             ),
             navigator.mediaSession.setActionHandler(
               "nexttrack",
-              (e) => !!o && o.click()
+              c
+                ? (e) => {
+                    c && c.click();
+                  }
+                : null
             );
         }
       };
-      i(), window.setInterval(i, 2e3);
-      r &&
+      c(), window.setInterval(c, 2e3);
+      i &&
         (function () {
           if ("/user/update" == location.pathname) {
             let e = (e) => {
               let t = document.querySelector("#baseBox");
               if (t) {
-                let e = `<div class="item"><h3><span class="f-fs1">&#x8f85;&#x52a9;&#x811a;&#x672c;&#x8bbe;&#x7f6e;</span><span class="sub s-fc3">&#x8f85;&#x52a9;&#x811a;&#x672c;&#x7248;&#x672c;&#xff1a;0.1.5-0.7.12</span><span class="sub s-fc3">&#x6837;&#x5f0f;&#x8868;&#x7248;&#x672c;&#xff1a;${a}</span></h3><ul class="n-plist n-plist-1">{1}</ul></div>`,
-                  n =
+                let e = `<div class="item"><h3><span class="f-fs1">&#x8f85;&#x52a9;&#x811a;&#x672c;&#x8bbe;&#x7f6e;</span><span class="sub s-fc3">&#x8f85;&#x52a9;&#x811a;&#x672c;&#x7248;&#x672c;&#xff1a;0.1.6-0.7.13</span><span class="sub s-fc3">&#x6837;&#x5f0f;&#x8868;&#x7248;&#x672c;&#xff1a;${s}</span></h3><ul class="n-plist n-plist-1">{1}</ul></div>`,
+                  a =
                     '<li><label><input type="checkbox" class="f-rdi" {2}>{1}</label></li>',
-                  r = [
+                  n = [
                     {
                       label:
                         "&#x4f7f;&#x7528;&#x6837;&#x5f0f;&#x8868;&#x7684;&#x589e;&#x5f3a;&#x5916;&#x89c2;",
@@ -252,41 +352,41 @@
                     },
                     {
                       label:
-                        "&#x5728;&#x7cfb;&#x7edf;&#x4e2d;&#x663e;&#x793a;&#x5a92;&#x4f53;&#x4fe1;&#x606f;&#xff08;&#x9700;&#x8981;&#x64cd;&#x4f5c;&#x7cfb;&#x7edf;&#x652f;&#x6301;&#xff09;",
+                        "&#x542f;&#x7528;&#x5a92;&#x4f53;&#x952e;&#x63a7;&#x5236;&#x548c;&#x5a92;&#x4f53;&#x4fe1;&#x606f;&#x663e;&#x793a;&#xff08;&#x9700;&#x8981;&#x64cd;&#x4f5c;&#x7cfb;&#x7edf;&#x652f;&#x6301;&#xff09;",
                       lsm: "wksmusicsessionmeta",
                       test: "'mediaSession' in navigator",
                     },
                   ],
                   i = "";
-                for (let e = 0; e < r.length; e++) {
-                  if (r[e].roi && !o) continue;
+                for (let e = 0; e < n.length; e++) {
+                  if (n[e].roi && !r) continue;
                   let t = !0;
-                  if (r[e].test) {
+                  if (n[e].test) {
                     t = !1;
                     try {
-                      t = !!window.eval(r[e].test);
+                      t = !!window.eval(n[e].test);
                     } catch (e) {}
                   }
                   if (!t) continue;
-                  let a = l(r[e].lsm);
-                  i += s(
-                    n,
-                    r[e].label,
-                    `${a ? "checked " : ""}data-wk-lsm="${r[e].lsm}"`
+                  let s = l(n[e].lsm);
+                  i += o(
+                    a,
+                    n[e].label,
+                    `${s ? "checked " : ""}data-wk-lsm="${n[e].lsm}"`
                   );
                 }
-                i = s(e, i);
+                i = o(e, i);
                 let c = document.createElement("div");
                 (c.className = "n-priv f-cb"),
                   (c.innerHTML = i),
                   c.addEventListener("change", (e) => {
                     let t = e.target.closest("[data-wk-lsm]"),
-                      n = t.dataset.wkLsm;
+                      a = t.dataset.wkLsm;
                     t &&
-                      n &&
+                      a &&
                       ((e, t) => {
                         window.localStorage && localStorage.setItem(e, t);
-                      })(n, t.checked ? "1" : "");
+                      })(a, t.checked ? "1" : "");
                   }),
                   t.appendChild(c);
               }
